cc6260e7577a4101aee96769fdfb0e2b33773f9b
sameeyaa <sameeya005@gmail.com>
2026-01-24 21:11:14 +0000

    filtering the data to only show 2025

--- mean_temp_graph.py
+++ mean_temp_graph.py
@@ -4,4 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #load csv file and get rid of rows not needed
-df = pd.read_csv("EnglandWeather.csv", skiprows = 5, sep = r"s+")
+df = pd.read_csv("EnglandWeather.csv", skiprows = 5, sep = r"\s+")
+
+#filter out data to only show April 2025-December 2025
+data_2025 = df[df["year"]== 2025]
+months = ["apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+monthly_temperature = data_2025[months].iloc[0]
