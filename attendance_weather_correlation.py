8a599706d66db29b2ea1654b24b353a20a7b55e0
sameeyaa <sameeya005@gmail.com>
2026-01-25 20:03:52 +0000

    import library

--- attendance_weather_correlation.py
+++ attendance_weather_correlation.py
@@ -1,2 +1,8 @@
 #testing the correlation and regression between weather and monthly attendance
 
+#load libraries
+import pandas as pd
+from pathlib import Path
+from scipy.stats import pearsonr
+import numpy as np
+
