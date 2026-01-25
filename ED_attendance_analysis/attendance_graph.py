e3ba76972d17aa83b586a75ce3fbab31fa324346
sameeyaa <sameeya005@gmail.com>
2026-01-25 18:29:55 +0000

    trying to combine all the files

--- ED_attendance_analysis/attendance_graph.py
+++ ED_attendance_analysis/attendance_graph.py
@@ -1,2 +1,14 @@
 #plot a graph displaying total A&E attendance from April 2025-December 2025
 
+#load necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+#load all CSV files from April to December
+dfs = []
+for file in Path(".").glob("*.csv"):
+    print(f"Reading: {file.name}")
+    dfs.append(pd.read_csv(file))
+
+df = pd.concat(dfs, ignore_index = True)
