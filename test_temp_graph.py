e868af3c1674383cc276d9c19ddbc132002f54aa
sameeyaa <sameeya005@gmail.com>
2026-01-25 17:47:34 +0000

    checking if output values are accurate

--- test_temp_graph.py
+++ test_temp_graph.py
@@ -14,3 +14,12 @@ def test_load_data():
     assert "year" in df.columns
     for col in ["apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]:
         assert col in df.columns
+
+#test to see if 9 values are returned as April-December is 9 months
+def test_2025_values():
+    df = load_data()
+    temps = get_monthly_temps(df, 2025)
+    assert len(temps) == 9
+    #check if temps contains a numeric value
+    assert pd.api.types.is_numeric_dtype(temps)
+
