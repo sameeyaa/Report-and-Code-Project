c853f6e5c31497f5f85c73985ea7d51ff081e0d2
sameeyaa <sameeya005@gmail.com>
2026-01-25 17:00:00 +0000

    adding more documentation

--- mean_temp_graph.py
+++ mean_temp_graph.py
@@ -1,4 +1,5 @@
 #plot a graph showing mean temperature for April 2025-December2025 
+#using the EnlandWeather.csv dataset
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,12 +9,18 @@ df = pd.read_csv("EnglandWeather.csv", skiprows = 5, sep = r"\s+")
 
 #filter out data to only show April 2025-December 2025
 data_2025 = df[df["year"]== 2025]
-months = ["apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+months = [
+    "apr", "may", "jun",
+    "jul", "aug", "sep", "oct",
+    "nov", "dec"
+    ]
 monthly_temperature = data_2025[months].iloc[0]
 
 #plot the mean temperature graph
-plt.figure()
+plt.figure(figsize = (12, 8))
 plt.plot(months, monthly_temperature, marker = 'o')
+
+#plot graph labels
 plt.xlabel("Month")
 plt.ylabel("Monthly Mean Temperature (degrees)")
 plt.title("2025 Mean Temperature in England")
