a1f8424e17eb20a7c08a78f4698a3c0003ed2e00
sameeyaa <sameeya005@gmail.com>
2026-01-26 17:58:56 +0000

    match the dates of the datasets

--- attendance_weather_correlation.py
+++ attendance_weather_correlation.py
@@ -13,3 +13,11 @@ ed_df = pd.read_csv(Path(__file__).resolve().parent / "combined_ed_totals.csv", 
 from testable_temp_graph import load_data, get_monthly_temps
 weather_df = load_data()
 temps = get_monthly_temps(weather_df, 2025)
+
+#use only April 2025- December 2025 and match the datasets
+months = pd.date_range("2025-04-01", "2025-12-01", freq = "MS")
+temp_df = pd.DataFrame({
+    "Month": months,
+    "Mean_Temperature": temps.values
+})
+
