14116c602889e7db35e2299aea72f1b2b65374ce
sameeyaa <sameeya005@gmail.com>
2026-01-26 17:55:39 +0000

    load both datasets

--- attendance_weather_correlation.py
+++ attendance_weather_correlation.py
@@ -6,3 +6,10 @@ from pathlib import Path
 from scipy.stats import pearsonr
 import numpy as np
 
+#load the combined data of Emergency Department attendances
+ed_df = pd.read_csv(Path(__file__).resolve().parent / "combined_ed_totals.csv", parse_dates = ["Month"]
+)
+#load temperature data
+from testable_temp_graph import load_data, get_monthly_temps
+weather_df = load_data()
+temps = get_monthly_temps(weather_df, 2025)
