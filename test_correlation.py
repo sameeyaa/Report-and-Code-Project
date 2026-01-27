6eabfe709b7f7218457e8c0e8a4d7970ba15494a
sameeyaa <sameeya005@gmail.com>
2026-01-27 19:52:50 +0000

    load temperature data set

--- test_correlation.py
+++ test_correlation.py
@@ -13,3 +13,9 @@ ed_df = pd.read_csv(
     "combined_ed_totals.csv",
     parse_dates=["Month"]
 )
+
+#get the temperature data
+from testable_temp_graph import load_data, get_monthly_temps
+weather_df = load_data()
+temps = get_monthly_temps(weather_df, 2025)
+
