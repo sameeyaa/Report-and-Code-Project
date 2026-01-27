a8957d69eaa014472515f8169d2b3d66572b512e
sameeyaa <sameeya005@gmail.com>
2026-01-27 16:16:00 +0000

    load the historical data

--- forecasting_attendance.py
+++ forecasting_attendance.py
@@ -7,3 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
+
+#use historical data
+df = pd.read_csv(Path(__file__).resolve().parent / "combined_ed_totals.csv",
+                 parse_dates = ["Month"]).sort_values("Month")
