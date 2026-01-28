ef3be300274d5a3c459651e0d2beef6df46554c6
sameeyaa <sameeya005@gmail.com>
2026-01-28 19:51:38 +0000

    load csv file

--- percentage_change.py
+++ percentage_change.py
@@ -4,3 +4,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+
+#load dataset
+df = pd.read_csv("combined_ed_totals.csv",
+                 parse_dates = ["Month"]).sort_values("Month")
+
