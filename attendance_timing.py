c717dac052f6068761f43ce55c4258b43b6d1a31
sameeyaa <sameeya005@gmail.com>
2026-01-28 12:55:47 +0000

    specify time column

--- attendance_timing.py
+++ attendance_timing.py
@@ -6,5 +6,12 @@ import matplotlib.pyplot as plt
 
 #load csv file but skip rows that are simply text
 df = pd.read_csv("attendance by time of day.csv", skiprows = 9) 
-print(df.head())
+#print(df.head())
+
+df = df[df["Measure Type"].str.contains("Number of A&E" , na= False)]
+
+#select time columns
+#classify ending with :00 as time
+time_column = [c for c in df.columns if c.endswith(":00")]
+df[time_column] = df[time_column].replace(",", "", regex = True).astype(int)
 
