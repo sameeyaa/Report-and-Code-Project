344a9a74c0ef66a8da335cd854fe5f6d0fbe304b
sameeyaa <sameeya005@gmail.com>
2026-01-28 13:09:37 +0000

    same initial code as timing graph

--- weekendvsweekday.py
+++ weekendvsweekday.py
@@ -1,2 +1,16 @@
 #bar chart to see whether weekdays or weekends have higher attendances
 
+#import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#load csv file but skip rows that are simply text
+df = pd.read_csv("attendance by time of day.csv", skiprows = 9) 
+#print(df.head())
+
+df = df[df["Measure Type"].str.contains("Number of A&E" , na= False)]
+
+#select time columns
+#classify ending with :00 as time
+time_column = [c for c in df.columns if c.endswith(":00")]
+df[time_column] = df[time_column].replace(",", "", regex = True).astype(int)
