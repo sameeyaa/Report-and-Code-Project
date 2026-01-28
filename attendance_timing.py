29022b62a22d27ddf63c8282b3e23db241cdc9de
sameeyaa <sameeya005@gmail.com>
2026-01-28 12:52:22 +0000

    skip rows

--- attendance_timing.py
+++ attendance_timing.py
@@ -4,5 +4,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("attendance by time of day.csv")
+#load csv file but skip rows that are simply text
+df = pd.read_csv("attendance by time of day.csv", skiprows = 9) 
 print(df.head())
+
