a53fec9a0aba7e6fcdaba059f8b8e96386f3ef6e
sameeyaa <sameeya005@gmail.com>
2026-01-28 12:59:00 +0000

    plot graph

--- attendance_timing.py
+++ attendance_timing.py
@@ -15,3 +15,14 @@ df = df[df["Measure Type"].str.contains("Number of A&E" , na= False)]
 time_column = [c for c in df.columns if c.endswith(":00")]
 df[time_column] = df[time_column].replace(",", "", regex = True).astype(int)
 
+#plot a line graph
+total = df[time_column].sum()
+plt.figure(figsize = (12,8))
+plt.plot(time_column, total)
+plt.xticks(rotation=90)
+
+#add labels
+plt.title("ED Attendances by time of day 204/25")
+plt.xlabel("Time of day (hours)")
+plt.ylabel("Number of Attendances")
+plt.show()
